990db358cc98d21515e1dc689e162c4ffa152ae4
Boy Next Door ♂ <873227635@qq.com>
2023-10-16 22:07:11 +0800

    feat: LCJ

--- main.cpp
+++ main.cpp
@@ -14,10 +14,16 @@ void Zhm()
     std::cout<<"The Zhu"<<std::endl;
 }
 
+void LCJ()
+{
+    std::cout<<"LCJ"<<std::endl;
+}
+
 int main()
 {
     std::cout<<"Hello"<<std::endl;
     std::cout<<"World"<<std::endl;
     LM();
     Zhm();
+    LCJ();
 }
