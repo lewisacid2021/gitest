260fd6e82dc1045db57d04bcf8c2d44787cdec64
wang <873227635@qq.com>
2023-10-16 22:00:32 +0800

    feat: Zhm

--- main.cpp
+++ main.cpp
@@ -4,8 +4,14 @@
 #include<bits/stdc++.h>
 #endif
 
+void Zhm()
+{
+    std::cout<<"The Zhu"<<std::endl;
+}
+
 int main()
 {
     std::cout<<"Hello"<<std::endl;
     std::cout<<"World"<<std::endl;
+    Zhm();
 }
