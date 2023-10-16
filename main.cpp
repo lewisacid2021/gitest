b35b31608a2b6688bb4fdfdfd6ea91538928637a
Boy Next Door ♂ <873227635@qq.com>
2023-10-16 22:00:09 +0800

    feat: LM

--- main.cpp
+++ main.cpp
@@ -4,8 +4,14 @@
 #include<bits/stdc++.h>
 #endif
 
+void LM()
+{
+    std::cout<<"LMeng"<<std::endl;
+}
+
 int main()
 {
     std::cout<<"Hello"<<std::endl;
     std::cout<<"World"<<std::endl;
+    LM();
 }
